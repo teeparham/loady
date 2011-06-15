51d45fe9bd31634270e016fa364a54893b56ae58
Tee Parham <tee@stackpilot.com>
2011-06-15 17:13:02 -0600

    add test for tab-delimited file

--- test/test_csv_loader.rb
+++ test/test_csv_loader.rb
@@ -32,6 +32,20 @@ class CsvLoaderTest < Test::Unit::TestCase
       assert_equal monkeys[9][:name], "King Kong", "last row name"
       assert_equal monkeys[9][:year], "1933", "last row year"
     end
+    
+    should "read file3, a tab-delimited file" do
+      monkeys = []
+
+      Loady.csv "test/csv/file3.dat", :skip_first_row => true, :col_sep => "\t"  do |row|
+        monkeys << { :name => row[0], :year => row[1] }
+      end
+
+      assert_equal monkeys.count, 10, "total rows read"
+      assert_equal monkeys[0][:name], "Bubbles", "first row name"
+      assert_equal monkeys[0][:year], "2000", "first row year"
+      assert_equal monkeys[9][:name], "King Kong", "last row name"
+      assert_equal monkeys[9][:year], "1933", "last row year"
+    end
        
   end
 
