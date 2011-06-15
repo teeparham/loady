7e34600b27b9267606152cf2391ebb1e80f7c610
Tee Parham <tee@stackpilot.com>
2011-06-15 17:12:42 -0600

    pass-through CSV options, remove unnecessary exception handling

--- lib/loady/csv_loader.rb
+++ lib/loady/csv_loader.rb
@@ -4,9 +4,11 @@ require 'logger'
 module Loady
   class CsvLoader
 
-    # options:
+    # valid options:
     #   :skip_first_row => true                       -- default = false
     #   :logger => Logger.new('/somewhere/file.log')  -- default = Logger.new(STDOUT)
+    #   plus any valid options you can pass to CSV.new: 
+    #     see http://www.ruby-doc.org/stdlib/libdoc/csv/rdoc/classes/CSV.html#M000190
     def self.read(filename, options={})
       success = 0
       warning = 0
@@ -18,42 +20,39 @@ module Loady
         logger.datetime_format = "%H:%M:%S"
       end
 
+      f = File.new(filename)
+      f.readline if options[:skip_first_row]
+      
+      [:logger, :skip_first_row].each {|d| options.delete(d) }
+      
+      line_number = 0
+
       begin
-        f = File.new(filename)
-        f.readline if options[:skip_first_row]
+        while (line = f.readline)
+          begin
+            line_number += 1
 
-        line_number = 0
+            row = CSV.parse(line, options)[0]
 
-        begin
-          while (line = f.readline)
-            begin
-              line_number += 1
+            unless row.empty?
+              yield row
 
-              row = CSV.parse(line)[0]
-
-              unless row.empty?
-                yield row
-
-                success += 1
-              end
-            rescue Exception => message
-              warning += 1
-              logger.warn "#{message.to_s.gsub("line 1", "line #{line_number}")}\n#{line}"
+              success += 1
             end
+          rescue Exception => message
+            warning += 1
+            logger.warn "#{message.to_s.gsub("line 1", "line #{line_number}")}\n#{line}"
           end
-        rescue EOFError
-          f.close
-        rescue Exception => message
-          warning += 1
-          line_number += 1
-          logger.error "#{message.to_s + " - at line #{line_number}"}\n#{line}"
         end
-
-        logger.info "Finished. Loaded #{success} rows. #{warning} unprocessed rows."
-
+      rescue EOFError
+        f.close
       rescue Exception => message
-        logger.fatal "Unhandled error: \n#{message}\n"
+        warning += 1
+        line_number += 1
+        logger.error "#{message.to_s + " - at line #{line_number}"}\n#{line}"
       end
+
+      logger.info "Finished. Loaded #{success} rows. #{warning} unprocessed rows."
     end
         
   end
