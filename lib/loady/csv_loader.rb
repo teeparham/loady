09dd484f0dd8c9ab650a4618395fc2961c5cec93
Tee Parham <tee@neighborland.com>
2013-01-27 18:23:57 -0700

    move counts to instance variables, init in initializer

--- lib/loady/csv_loader.rb
+++ lib/loady/csv_loader.rb
@@ -9,47 +9,48 @@ module Loady
       end
     end
 
+    def initialize
+      @success = 0
+      @warning = 0
+      @line_number = 0
+    end
+
     # valid options:
     #   skip_first_row: true                       -- default = false
     #   logger: Logger.new('/somewhere/file.log')  -- default = Logger.new(STDOUT)
     #   plus any valid options you can pass to CSV.new:
     #     see http://www.ruby-doc.org/stdlib/libdoc/csv/rdoc/classes/CSV.html#M000190
     def read(filename, options={})
-      success = 0
-      warning = 0
-
       logger = options.delete(:logger) || default_logger
 
       f = File.new(filename)
       f.readline if options.delete(:skip_first_row)
 
-      line_number = 0
-
       begin
         while (line = f.readline)
           begin
-            line_number += 1
+            @line_number += 1
 
             row = Loady::AttributeArray.new CSV.parse(line, options)[0]
 
             unless row.empty?
               yield row
-              success += 1
+              @success += 1
             end
           rescue Exception => message
-            warning += 1
-            logger.warn "#{message.to_s.gsub("line 1", "line #{line_number}")}\n#{line}"
+            @warning += 1
+            logger.warn "#{message.to_s.gsub("line 1", "line #@line_number")}\n#{line}"
           end
         end
       rescue EOFError
         f.close
       rescue Exception => message
-        warning += 1
-        line_number += 1
-        logger.error "#{message.to_s} - at line #{line_number}\n#{line}"
+        @warning += 1
+        @line_number += 1
+        logger.error "#{message.to_s} - at line #@line_number\n#{line}"
       end
 
-      logger.info "Finished. Loaded #{success} rows. #{warning} unprocessed rows."
+      logger.info "Finished. Loaded #@success rows. #@warning unprocessed rows."
     end
 
 
